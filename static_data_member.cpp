b66f1848c9c1a655e75d377ef7852d5999b693be
Tanvi Khandelwal <tkhandelwal752@gmail.com>
2025-01-10 16:20:32 +0530

    Add files via upload

--- static_data_member.cpp
+++ static_data_member.cpp
@@ -3,9 +3,15 @@ using namespace std;
 class demo{
     public:
     static int abc;
+    demo(){
+        abc++;
+    }
 };
 int demo::abc=10;
 int main(){
 int a;
 cout<<"value of abc:"<<demo::abc<<endl;
+demo obj;
+cout<<"value of abc after one object:"<<demo::abc<<endl;
+return 0;
 }
